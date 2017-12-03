bffd829ac910be458ddc637f03b42692c1732ab2
Oli Passey <oli@hedgerows.net>
2017-12-03 20:22:30 +0000

    refresh all fixes

--- plugin.audio.kissfm/addon.py
+++ plugin.audio.kissfm/addon.py
@@ -15,7 +15,7 @@ li = xbmcgui.ListItem('Kisstory', iconImage='http://www.bauermedia.co.uk/bauer-e
 li.setProperty('fanart_image', 'https://www.taolasvegas.com/files/2016/06/TAO_nightclub-2016-2.jpg')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
-url = 'http://live-kiss.sharp-stream.com/kissfresh.aac'
+url = 'http://live-bauer-al.sharp-stream.com/kissfresh.aac'
 li = xbmcgui.ListItem('Kiss Fresh', iconImage='http://ukradioonline.com/sites/default/files/radio/logos/logo-kiss-fresh.jpg', thumbnailImage='http://www.kissfm.md/web/img/site/logo.png')
 li.setProperty('fanart_image', 'https://www.taolasvegas.com/files/2016/06/TAO_nightclub-2016-2.jpg')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
