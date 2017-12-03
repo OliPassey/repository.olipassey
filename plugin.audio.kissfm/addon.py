9a44b1ea476428302817911dc4d82de37546b9fc
Oli Passey <oli@hedgerows.net>
2017-12-03 19:55:03 +0000

    fix the dead addons
    attempt1 anyway

--- plugin.audio.kissfm/addon.py
+++ plugin.audio.kissfm/addon.py
@@ -5,15 +5,19 @@ import xbmcplugin
 addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'audio')
-
-url = 'http://tx.whatson.com/icecast.php?i=kisstorylow.mp3'
-li = xbmcgui.ListItem('Kisstory', iconImage='http://whitenoisehq.co.uk/kisstory.png', thumbnailImage='http://whitenoisehq.co.uk/kisstory.png')
-li.setProperty('fanart_image', 'http://whitenoisehq.co.uk/kissbg.jpg')
+url = 'http://live-kiss.sharp-stream.com/kiss100.aac'
+li = xbmcgui.ListItem('Kiss FM UK - National', iconImage='http://www.industrytrust.co.uk/wp-content/uploads/2014/01/Kiss-logo.jpg', thumbnailImage='http://www.kissfm.md/web/img/site/logo.png')
+li.setProperty('fanart_image', 'https://www.taolasvegas.com/files/2016/06/TAO_nightclub-2016-2.jpg')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
-url = 'http://tx.whatson.com/icecast.php?i=kissnationallow.mp3'
-li = xbmcgui.ListItem('Kiss FM UK - National', iconImage='http://whitenoisehq.co.uk/kisslogo.png', thumbnailImage='http://whitenoisehq.co.uk/kisslogo.png')
-li.setProperty('fanart_image', 'http://whitenoisehq.co.uk/kissbg.jpg')
+url = 'http://live-bauer-al.sharp-stream.com/kisstory.aac'
+li = xbmcgui.ListItem('Kisstory', iconImage='http://www.bauermedia.co.uk/bauer-emptor/image/upload/c_fit,h_200,w_300/v1437411903/bauer-emptor-keystone-test/brands/logo/oovzr5nyuzckztyolaim.png', thumbnailImage='http://www.kissfm.md/web/img/site/logo.png')
+li.setProperty('fanart_image', 'https://www.taolasvegas.com/files/2016/06/TAO_nightclub-2016-2.jpg')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+url = 'http://live-kiss.sharp-stream.com/kissfresh.aac'
+li = xbmcgui.ListItem('Kiss Fresh', iconImage='http://ukradioonline.com/sites/default/files/radio/logos/logo-kiss-fresh.jpg', thumbnailImage='http://www.kissfm.md/web/img/site/logo.png')
+li.setProperty('fanart_image', 'https://www.taolasvegas.com/files/2016/06/TAO_nightclub-2016-2.jpg')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
--- plugin.audio.rinsefm/addon.py
+++ plugin.audio.rinsefm/addon.py
@@ -6,7 +6,7 @@ addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-url = 'http://podcast.dgen.net:8000/rinseradio.m3u'
+url = 'http://streamer.dgen.net:8000/rinseradio.m3u'
 li = xbmcgui.ListItem('Rinse.FM', iconImage='http://in-reach.co.uk/wp-content/uploads/2014/02/rinse-fm-logo-620x400.png', thumbnailImage='http://in-reach.co.uk/wp-content/uploads/2014/02/rinse-fm-logo-620x400.png')
 li.setProperty('fanart_image', 'http://in-reach.co.uk/wp-content/uploads/2014/02/rinse-fm-logo-620x400.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
